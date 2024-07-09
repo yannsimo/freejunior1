971e0c5f27eca0aab3342e5046a3ed9c653bd586
unknown <yann simo>
2024-07-10 00:46:20 +0200

    Admin

--- StudentApp/admin.py
+++ StudentApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from StudentApp.models import Company, School, Specialty, Program, Subject, Student , Mission, Comment
+from Accounts.models import User
+admin.site.register(School)
+admin.site.register(Specialty)
+admin.site.register(Program)
+admin.site.register(Subject)
+admin.site.register(Student)
 
-# Register your models here.
+admin.site.register(Company)
+admin.site.register(Mission)
+admin.site.register(Comment)
+admin.site.register(User)
